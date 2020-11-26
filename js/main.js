5bff8ab6dfd915e030bdb3e6658a807848a8c0d4
LeanC100 <46855569+LeanC100@users.noreply.github.com>
2020-11-25 21:07:33 -0300

    Parte principal realizada -- pagina no terminada

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+ // Menu
+function Menu (params) {
+    
+}
+
+// hover y no hover
+function nosotros_hover(i, s, t) {
+    var item = i, svg = s, texto = t;
+    var nosotros_div_item = document.getElementById(item);
+    var nosotros_svg = document.getElementById(svg);
+    var nosotros_div_texto = document.getElementById(texto);
+
+    nosotros_div_item.style.backgroundColor = "#e08709";
+    nosotros_svg.style.color = "#fff";
+    nosotros_div_texto.style.color = "#fff";
+}
+function nosotros_no_hover(i, s, t) {
+    var item = i, svg = s, texto = t;
+    var nosotros_div_item = document.getElementById(item);
+    var nosotros_svg = document.getElementById(svg);
+    var nosotros_div_texto = document.getElementById(texto);
+
+    nosotros_div_item.style.backgroundColor = "#fff";
+    nosotros_svg.style.color = "#e08709";
+    nosotros_div_texto.style.color = "#000";
+}
